6280d972522bc0cca3713b5f174924fbf6f9667b
AnilaMacha <anilamacha2000@gmail.com>
2024-06-14 23:04:42 -0500

    episode-03

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 
-const heading=React.createElement("h1",{id:"head"}, "Hello React🚀")
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const Title= () => (
+  <h1 className="head">React JS</h1>
+);
+const num=100;
+const Heading= () => (
+  <div className="Container">
+    <Title/>
+    <h1>{num}</h1>
+    <h1 className="head1">Namasthe React🚀</h1>
+  </div>
+  
+);
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(<Heading/>)
