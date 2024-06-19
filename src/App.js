912638ff6a2e7be7b05a50908fbb8eee91353da6
AnilaMacha <anilamacha2000@gmail.com>
2024-06-19 08:45:23 -0500

    episode-05

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header.js"
+import Body from "./components/Body.js"
+
+
+/**
+ * Header
+ * -logo
+ * -nav items
+ * Body
+ * -search
+ * -Restaurant Container
+ * --Restaurant card
+ * Footer
+ * -copyright
+ * -links
+ * -address
+ * -contact
+ */
+
+
+
+
+const AppLayout=() => {
+  return(
+    <div className="app">
+      <Header/>
+      <Body/>
+    </div>
+  )
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<AppLayout/>)
+
